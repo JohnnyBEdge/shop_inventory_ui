3fec60b097e912571876362daf0e6ee4f47ab1af
John Martin <jrmartin182@gmail.com>
2020-06-01 14:13:44 -0600

    added logos and styled main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import Login from './Login';
+import Logo1 from '../styling/imgs/logo1.png';
+import Logo2 from '../styling/imgs/logo2.png';
+import Logo3 from '../styling/imgs/logo3.png';
+import Logo4 from '../styling/imgs/logo4.png';
+import Logo5 from '../styling/imgs/logo5.png';
+import Logo6 from '../styling/imgs/logo6.png';
+import Logo7 from '../styling/imgs/logo7.png';
+import Logo8 from '../styling/imgs/logo8.png';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Link from '@material-ui/core/Link';
@@ -13,16 +21,28 @@ const useStyles = makeStyles({
     },
     container: {
         width: "50%",
-        border: "1px black solid",
         height: "80vh",
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
-        padding: "30px 50px"
+        padding: "30px 50px",
+        color: "#4E4E4E",
+        fontSize: "22px",
+        fontWeight: "300",
+        lineHeight: "1.5"
     },
-    signIn: {
-        border: "solid black 1px"
+    logos: {
+        display: "grid",
+        gridTemplateColumns: "repeat(3, 1fr)",
+        width: "100%"
+    },
+    logo: {
+        width: "1fr",
+        height: "100px"
+    },
+    image: {
+        width: "90%"
     }
 });
 
@@ -33,7 +53,34 @@ const Main = () => {
     return (
         <div id="main_container" className={classes.main}>
             <div id="ad_container" className={classes.container}>
+                <p>All the brands you love, at prices you can afford!</p>
+                <div className={classes.logos}>
+                    <div className={classes.logo}>
+                        <img src={Logo1} className={classes.image}/>
+                    </div>
+                    <div className={classes.logo}>
+                        <img src={Logo2} className={classes.image} />
+                    </div>
+                    <div className={classes.logo}>
+                        <img src={Logo3} className={classes.image} />
+                    </div>
+                    <div className={classes.logo}>
+                        <img src={Logo4} className={classes.image} />
+                    </div>
+                    <div className={classes.logo}>
+                        <img src={Logo5} className={classes.image} />
+                    </div>
+                    <div className={classes.logo}>
+                        <img src={Logo6} className={classes.image} />
+                    </div>
+                    <div className={classes.logo}>
+                        <img src={Logo7} className={classes.image} />
+                    </div>
+                    <div className={classes.logo}>
+                        <img src={Logo8} className={classes.image} />
+                    </div>
 
+                </div>
             </div>
             <div id="welcome_container" className={classes.container}>
                 <h1>Welcome</h1>
