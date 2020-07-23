fb4b703d551724f8f7ebd67b5f90a02820ab3033
John Martin <jrmartin182@gmail.com>
2020-07-23 11:39:37 -0600

    Updates

---
+++ precache-manifest.8a648e7c676dbd36f54899e509aa0384.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edf971adc2dc649e4e10b059f4a76a30",
+    "url": "/shopv2/index.html"
+  },
+  {
+    "revision": "3a777095e17144a6b3e3",
+    "url": "/shopv2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1bad28e17cbe214ea1f9",
+    "url": "/shopv2/static/js/2.37648b4a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/shopv2/static/js/2.37648b4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a777095e17144a6b3e3",
+    "url": "/shopv2/static/js/main.b52bf492.chunk.js"
+  },
+  {
+    "revision": "5bf9dd52b3ec2a7c8624",
+    "url": "/shopv2/static/js/runtime-main.5d5c48d8.js"
+  }
+]);
